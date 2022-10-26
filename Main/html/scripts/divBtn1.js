168196c5d014afcfab5b30b1fa1c99c0583dffa7
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2022-10-26 21:18:57 +0300

    Small restruct and add button

---
+++ Main/html/scripts/divBtn1.js
@@ -0,0 +1,8 @@
+document.getElementById("btn1").onmousedown = function(event) {
+    if (event.button == 0) { //if (event.which == 3) {
+        document.getElementById('btn1').style="background-color: #b5b5b5a1;"
+    }
+    if (event.button == 2) { //if (event.which == 3) {
+        document.getElementById('btn1').style="background-color: rgb(32, 33, 36);"
+    }
+}
