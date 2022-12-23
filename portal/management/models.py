aa4fde524a4df16c452322b805eff47dfeb38d63
AdLeGeR <dmitr1effal@yandex.ru>
2022-12-23 21:51:27 +0300

    add models User, Student, Teacher, Project, Request

--- portal/management/models.py
+++ portal/management/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User (models.Model):
+    name=models.CharField(max_length=30)
+    lastName=models.CharField(max_length=30)
+    fatherName=models.CharField(max_length=30)
+    access=models.CharField(max_length=30)
+
+class Student(User):
+    def __init__(self, **kwargs):
+        super(student, self).__init__(**kwargs)
+        self.access="student"
+
+class Teacher(User):
+    def __init__(self, **kwargs):
+        super(Teacher, self).__init__(**kwargs)
+        self.access="teacher"
--- portal/projects/models.py
+++ portal/projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from management.models import Teacher, Student
 
-# Create your models here.
+class Project(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE())
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE())
+    name = models.CharField(max_length=30)
+    __statuses = ["on work", "send to verification", "done"]
+    __status = models.CharField(max_length=30)
+    def setStatus(self, n):
+        if n in self.__statuses:
+            self.status = n
+    def getStatus(self):
+        return self.status
+
+class Request(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE())
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE())
