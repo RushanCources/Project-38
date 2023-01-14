357492d51d3aa61215de19c41f290b00e407ac6d
AdLeGeR <dmitr1effal@yandex.ru>
2023-01-14 16:04:51 +0300

    delete model Request, correct model Project

--- portal/projects/models.py
+++ portal/projects/models.py
@@ -2,17 +2,14 @@ from django.db import models
 from management.models import Teacher, Student
 
 class Project(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE())
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE())
+    student = models.ForeignKey(Student, on_delete=models.DO_NOTHING())
+    teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING())
     name = models.CharField(max_length=30)
-    __statuses = ["on work", "send to verification", "done"]
-    __status = models.CharField(max_length=30)
+    _statuses = ["send request", "on work", "send to verification", "done"]
+    _status = models.CharField(max_length=30)
     def setStatus(self, n):
-        if n in self.__statuses:
+        if n in self._statuses:
             self.status = n
     def getStatus(self):
         return self.status
 
-class Request(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE())
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE())
