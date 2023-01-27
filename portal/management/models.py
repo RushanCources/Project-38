22cf3032f314531de7350b9953c1743db2f62a80
d1ffy <helpersteam96@inbox.ru>
2023-01-27 20:42:41 +0300

    Russification

--- portal/management/models.py
+++ portal/management/models.py
@@ -1,4 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(User,self).__init__(*args, **kwargs)
+        self._meta.get_field('username').verbose_name="Логин"
+        self._meta.get_field('password').verbose_name="Пароль"
+
+
