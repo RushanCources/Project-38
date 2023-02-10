3ae2bb1288e039b35378e0e9eccc7c454b9db6fd
AdLeGeR <dmitr1effal@yandex.ru>
2023-02-10 18:07:34 +0300

    fix conflicts 2

--- portal/management/views.py
+++ portal/management/views.py
@@ -1,6 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
-def index(request):
-    return HttpResponse("Что-то")
 
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            messages.success(request, 'Аккаунт успешно создан')
+            return redirect('register')
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'user_form': user_form})
