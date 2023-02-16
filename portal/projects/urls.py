765c84f2c22a8de3e22fe7111bf5085472fb1f68
XameleonchikGreen <budnikov_nikita@list.ru>
2023-02-16 19:56:15 +0300

    Базовый интерфейс

--- portal/projects/urls.py
+++ portal/projects/urls.py
@@ -4,4 +4,5 @@ from projects import views
 urlpatterns = [
     path('', views.index, name='projects'),
     path('account/', views.account, name='account'),
+    path('create/', views.create, name='create'),
 ]
--- portal/projects/views.py
+++ portal/projects/views.py
@@ -2,7 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Что-то")
+    return render(request, "projects/index.html")
 
 def account(request):
     return HttpResponse("Профиль")
+
+def create(request):
+    return render(request, "projects/create.html")
+
+def postcreate(request):
+    student = request.POST.get("student")
+    teacher = request.POST.get("teacher")
+    name = request.POST.get("name")
+    return render(request, "projects/success.html")
