a87304f0c2f02d471c0424f1e015b15b08f6afc4
AdLeGeR <dmitr1effal@yandex.ru>
2023-02-23 12:38:50 +0300

    ограничил доступ к подача заявки на проект

--- portal/projects/views.py
+++ portal/projects/views.py
@@ -8,10 +8,16 @@ def account(request):
     return HttpResponse("Профиль")
 
 def create(request):
-    return render(request, "projects/create.html")
+    if request.user.is_authenticated:
+        if request.user.role == "Ученик":
+            return render(request, "projects/create.html")
+    return render(request, "NotEnoughPermissions.html")
 
 def postcreate(request):
-    student = request.POST.get("student")
+    if not request.user.is_authenticated:
+        return render(request, "NotEnoughPermissions.html")
+    if request.user.role != "Ученик":
+        return render(request, "NotEnoughPermissions.html")
     teacher = request.POST.get("teacher")
     name = request.POST.get("name")
     return render(request, "projects/success.html")
