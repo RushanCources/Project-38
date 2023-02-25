a4699e33871423d7ee7cf45992148d73c11bee18
happyzxzxz <eganov2006@mail.ru>
2023-02-25 18:33:25 +0300

    Added functional of editing existing announcements and allowed it only for admins and teachers groups

--- portal/announcements/urls.py
+++ portal/announcements/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='announcements'),
     path('redactor', views.redactor, name='redactor'),
-    path('createannouncement', views.createannouncement, name='createannouncement')
+    path('createannouncement', views.createannouncement, name='createannouncement'),
+    path('editor/<int:id>', views.editor, name='editor'),
+    path('editor/editannouncement/<int:id>', views.editannouncement, name='editannouncement')
 ]
--- portal/announcements/views.py
+++ portal/announcements/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponsePermanentRedirect
 from .models import Announcement
-from datetime import date
+from datetime import date, datetime
 from .decorators import allowed_users
 
 
@@ -19,6 +19,7 @@ def redactor(request):
 
 @allowed_users(allowed_roles=['Teacher', 'admin'])
 def createannouncement(request):
+
     title = request.POST.get("title")
     body = request.POST.get("body")
     is_pinned = request.POST.get("is_pinned")
@@ -28,4 +29,31 @@ def createannouncement(request):
     de = de.split("-")
 
     announcement = Announcement.objects.create(title=str(title), body=str(body), is_pinned=bool(is_pinned), date_of_expiring=date(int(de[0]), int(de[1]), int(de[2])), author=author)
+    return HttpResponsePermanentRedirect('/announcements')
+
+
+@allowed_users(allowed_roles=['Teacher', 'admin'])
+def editor(request, id):
+    data = {'announcement': Announcement.objects.get(id=id),
+            'date_of_expiring': str(Announcement.objects.get(id=id).date_of_expiring)[:10],
+            }
+
+    return render(request, 'announcements/editor.html', context=data)
+
+
+@allowed_users(allowed_roles=['Teacher', 'admin'])
+def editannouncement(request, id):
+    announcement = Announcement.objects.get(id=id)
+    de = request.POST.get("date_of_expiring").split("-")
+    date_of_expiring = date(int(de[0]), int(de[1]), int(de[2]))
+    is_pinned = request.POST.get("is_pinned")
+    if is_pinned == '': is_pinned = 0
+
+    announcement.title = request.POST.get("title")
+    announcement.body = request.POST.get("body")
+    announcement.is_pinned = is_pinned
+    announcement.date_of_expiring = date_of_expiring
+
+    announcement.save()
+
     return HttpResponsePermanentRedirect('/announcements')
