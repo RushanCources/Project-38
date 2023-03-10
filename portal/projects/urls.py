a0c479159429bab6c3650984fbfb4efcbbcd6e68
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2023-03-10 20:59:17 +0300

    fx

--- portal/projects/urls.py
+++ portal/projects/urls.py
@@ -3,9 +3,5 @@ from projects import views
 
 urlpatterns = [
     path('', views.index, name='projects'),
-    path('create/', views.create, name='create'),
-    path('create/postcreate/', views.postcreate, name='postcreate'),
-    path('serchscript', views.jslibs, name='select2'),
-    path('jquery', views.jslibs, name='jquery'),
-    path('select2css', views.csslibs, name='selec2css')
+    path('account/', views.account, name='account'),
 ]
