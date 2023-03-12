b58c3838a3f61d6a3623194c222c64cf4557a00e
happyzxzxz <eganov2006@mail.ru>
2023-03-12 16:40:47 +0300

    Added try/except constructions and some restrictions to views.py

--- portal/announcements/views.py
+++ portal/announcements/views.py
@@ -13,13 +13,16 @@ def index(request):
     return render(request, 'dec/dec.html')
 
 
-@allowed_users(allowed_roles=['Teacher', 'admin'])
+#@allowed_users(allowed_roles=['Teacher', 'admin'])
 def redactor(request):
     return render(request, "announcements/redactor.html")
 
 
-@allowed_users(allowed_roles=['Teacher', 'admin'])
+#@allowed_users(allowed_roles=['Teacher', 'admin'])
 def createannouncement(request):
+
+    if request.method != "POST":
+        return HttpResponsePermanentRedirect("/announcements")
 
     title = request.POST.get("title")
     body = request.POST.get("body")
@@ -43,39 +46,52 @@ def createannouncement(request):
     return HttpResponsePermanentRedirect('/announcements')
 
 
-@allowed_users(allowed_roles=['Teacher', 'admin'])
+#@allowed_users(allowed_roles=['Teacher', 'admin'])
 def editor(request, id):
-    data = {'announcement': Announcement.objects.get(id=id),
-            'date_of_expiring': str(Announcement.objects.get(id=id).date_of_expiring)[:10],
-            }
+    try:
+        data = {'announcement': Announcement.objects.get(id=id),
+                'date_of_expiring': str(Announcement.objects.get(id=id).date_of_expiring)[:10],
+                }
 
-    return render(request, 'announcements/editor.html', context=data)
+        return render(request, 'announcements/editor.html', context=data)
+
+    except Announcement.DoesNotExist:
+        return HttpResponse('Объявление не найдено')
 
 
-@allowed_users(allowed_roles=['Teacher', 'admin'])
+#@allowed_users(allowed_roles=['Teacher', 'admin'])
 def editannouncement(request, id):
-    announcement = Announcement.objects.get(id=id)
-    de = request.POST.get("date_of_expiring").split("-")
-    date_of_expiring = date(int(de[0]), int(de[1]), int(de[2]))
-    is_pinned = request.POST.get("is_pinned")
-    tags_str = request.POST.get("tags")
-    if is_pinned == '': is_pinned = 0
 
-    my_tags = []
+    try:
 
-    tags_list = tags_str.split('\r\n')
+        if request.method != "POST":
+            return HttpResponsePermanentRedirect('/announcements')
 
-    for name in tags_list:
-        tag_exm = Tag(name=name)
-        tag_exm.save()
-        my_tags.append(tag_exm)
+        announcement = Announcement.objects.get(id=id)
+        de = request.POST.get("date_of_expiring").split("-")
+        date_of_expiring = date(int(de[0]), int(de[1]), int(de[2]))
+        is_pinned = request.POST.get("is_pinned")
+        tags_str = request.POST.get("tags")
+        if is_pinned == '': is_pinned = 0
 
-    announcement.title = request.POST.get("title")
-    announcement.body = request.POST.get("body")
-    announcement.is_pinned = is_pinned
-    announcement.date_of_expiring = date_of_expiring
-    announcement.tags.set(my_tags)
+        my_tags = []
 
-    announcement.save()
+        tags_list = tags_str.split('\r\n')
 
-    return HttpResponsePermanentRedirect('/announcements')
+        for name in tags_list:
+            tag_exm = Tag(name=name)
+            tag_exm.save()
+            my_tags.append(tag_exm)
+
+        announcement.title = request.POST.get("title")
+        announcement.body = request.POST.get("body")
+        announcement.is_pinned = is_pinned
+        announcement.date_of_expiring = date_of_expiring
+        announcement.tags.set(my_tags)
+
+        announcement.save()
+
+        return HttpResponsePermanentRedirect('/announcements')
+
+    except Announcement.DoesNotExist:
+        HttpResponse('Объявление не найдено')
