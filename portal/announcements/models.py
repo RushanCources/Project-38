572ec1c784253cbb9a28acbbca974ba73fbd9367
happyzxzxz <eganov2006@mail.ru>
2023-03-22 15:50:18 +0300

    Deleted tags and added 'superuser' variable to the frontend

--- portal/announcements/models.py
+++ portal/announcements/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.conf import settings
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=50)
-
-
 class TimeStampMixin(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -20,7 +16,6 @@ class Announcement(TimeStampMixin):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     is_pinned = models.BooleanField(default=False)
     date_of_expiring = models.DateTimeField(null=True)
-    tags = models.ManyToManyField(Tag)
 
 
 class Image(models.Model):
