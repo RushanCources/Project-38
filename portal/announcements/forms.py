463a1b671c9c0355257242e8dc31230e7ebb91aa
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2023-04-14 16:48:16 +0300

    11

--- portal/announcements/forms.py
+++ portal/announcements/forms.py
@@ -5,10 +5,10 @@ from .models import Announcement
 
 
 class AnnouncementForm(forms.Form):
-    title = forms.CharField(label='', required=True, widget=forms.TextInput(attrs={'class': '', 'placeholder' : 'Имя объявления'}))
+    date_of_expiring = forms.DateField(label='', widget=forms.DateInput(attrs={'class': 'date', 'type': 'date'}), required=False)
+    title = forms.CharField(label='', required=True, widget=forms.TextInput(attrs={'class': 'title', 'placeholder' : 'Имя объявления'}))
     body = forms.CharField(label='', widget=forms.Textarea)
     is_pinned = forms.BooleanField(label='Закрепить', required=False)
-    date_of_expiring = forms.DateField(label='Актуально до', widget=forms.DateInput(attrs={'type': 'date'}), required=False)
     image_url = forms.FilePathField(label='Выбрать обложку', path='static/img/announcements/covers', required=False)
     files = forms.FileField(label='Прикрепить файлы', widget=forms.FileInput(attrs={'multiple': True}), required=False)
     file_id_to_delete = forms.IntegerField(widget=forms.HiddenInput(), required=False)
