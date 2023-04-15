78f359a479a172f0baecab0388fa8824b4da5c01
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2023-04-15 16:20:17 +0300

    init

--- portal/announcements/forms.py
+++ portal/announcements/forms.py
@@ -7,11 +7,11 @@ from .models import Announcement
 class AnnouncementForm(forms.Form):
     date_of_expiring = forms.DateField(label='', widget=forms.DateInput(attrs={'class': 'date', 'type': 'date'}), required=False)
     title = forms.CharField(label='', required=True, widget=forms.TextInput(attrs={'class': 'title', 'placeholder' : 'Имя объявления'}))
-    body = forms.CharField(label='', widget=forms.Textarea)
+    body = forms.CharField(label='', widget=forms.Textarea(attrs={'class': 'body'}))
     is_pinned = forms.BooleanField(label='Закрепить', required=False)
-    image_url = forms.FilePathField(label='Выбрать обложку', path='static/img/announcements/covers', required=False)
-    files = forms.FileField(label='Прикрепить файлы', widget=forms.FileInput(attrs={'multiple': True}), required=False)
-    file_id_to_delete = forms.IntegerField(widget=forms.HiddenInput(), required=False)
+    image_url = forms.FilePathField(label='Выбрать обложку', path='static/img/announcements/covers', required=False, widget=forms.TextInput(attrs={'class': 'imurl'}))
+    files = forms.FileField(label='Прикрепить файлы', widget=forms.FileInput(attrs={'multiple': True, 'class':"files"}), required=False)
+    file_id_to_delete = forms.IntegerField(widget=forms.HiddenInput(attrs={'class': 'fitd'}), required=False)
 
     def clean_date_of_expiring(self):
         exdate = self.cleaned_data.get('date_of_expiring')
