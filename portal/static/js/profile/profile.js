4ebf74b58dc8728630f2eb4f09dec0fc91c5605e
Denis Kabanin <kabanindenis9@gmail.com>
2023-04-16 17:40:28 +0300

    Продолжил реализацию страницы профиля для учителя

--- portal/static/js/profile/profile.js
+++ portal/static/js/profile/profile.js
@@ -1,3 +1,12 @@
+window.addEventListener('scroll', function() {
+    if (this.scrollY > 120) {
+        console.log ('test');
+        $('.main-info').addClass('main-info-fixed');
+    } else {
+        $('.main-info').removeClass('main-info-fixed');
+    }
+});
+
 $('.pencil').on('click', function(){
     $('.new-avatar-div').css({'display':'block'});
     $('.back-form').css({'display':'block'});
@@ -6,4 +15,28 @@ $('.pencil').on('click', function(){
 $('.new-avatar-close').on('click', function(){
     $('.new-avatar-div').css({'display':'none'});
     $('.back-form').css({'display':'none'});
+});
+
+$('.btn-projects').on('click', function() {
+    let ul = this.parentNode.childNodes[5];
+    let icon = this.parentNode.childNodes[3];
+    if ($(ul).hasClass('ul-open')) {
+        $(ul).removeClass('ul-open');
+        $(icon).css({'transform' : 'rotate(45deg)'});
+    } else {
+        $(ul).addClass('ul-open');
+        $(icon).css({'transform' : 'rotate(0deg)'});
+    }
+});
+
+$('.btn-new-projects').on('click', function() {
+    let ul = this.parentNode.parentNode.childNodes[5];
+    let icon = this.parentNode.parentNode.childNodes[3];
+    if ($(ul).hasClass('ul-open')) {
+        $(ul).removeClass('ul-open');
+        $(icon).css({'transform' : 'rotate(45deg)'});
+    } else {
+        $(ul).addClass('ul-open');
+        $(icon).css({'transform' : 'rotate(0deg)'});
+    }
 });
