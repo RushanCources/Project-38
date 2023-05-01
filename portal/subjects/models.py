95b8f3eeb7035e45645df7104564da94e19f477f
kie-06-forwork <125133234+Lexi-Wooo@users.noreply.github.com>
2023-05-01 17:10:22 +0300

    01.05.2023
    Сделал вывод тем на страницу, все работает

--- portal/subjects/models.py
+++ portal/subjects/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
 class Theme(models.Model):
-    themeNAME = models.CharField(max_length=100)  # Имя темы .. Хватит за глаза
-    themeAUTHOR = models.CharField(max_length=30)  # Автор (тот, кто создал тему) .. Самаркандий Самаркандович влез
-    themeSUPVISOR = models.CharField(max_length=30)  # Руководитель темы (преподаватель) .. Аналогично AUTHOR
-    themeTYPE = models.CharField(max_length=10)  # Тип темы [Проект] или [НОУ], размер не важен, список выпадающий
-    themeSUBJECT = models.CharField(max_length=20)  # Предметная область темы, опять же список выпадает
-    themeSTATUS = models.CharField(max_length=20)  # Статус темы [Свободна], [Занята], [Прошлых лет]
-    themeDESCRIPT = models.CharField(max_length=2000)  # Описание темы .. Доп. условия, и прочее, места хватить должно
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=30)
+    subject = models.CharField(max_length=20)
+    status = models.CharField(max_length=20)  # Статус темы [Свободна], [Занята], [Прошлых лет]
+    descript = models.CharField(max_length=2000)
--- portal/subjects/views.py
+++ portal/subjects/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Theme
 
+# Логин:lexi-wooo Пароль: 19042002
 
 def without_filter(request):
-    return render(request, 'theme_list/theme_list.html')
+
+    # theme1 = Theme.objects.filter(id=1).update(name="Сравнительная характеристика навозных и дождевых червей", author="Коновалов С. М", subject="Природоведение",
+    #  status="Свободна", descript="Рыбы любят навозных червей больше, чем дождевых (на последих ничо не клюет)")
+    # theme1.save()
+    # theme2 = Theme.objects.filter(id=2).update(name="Роль буржуазии в литературе XIX века", author="Еременко Д. Ф", subject="Литература",
+    #  status="Свободна", descript="В большинстве случаев мы считаем, что буржуазия была не очень приятным элементом общества, однако если посмотреть подробнее...")
+    # theme2.save()
+    
+    themes = Theme.objects.all()
+    return render(request, 'theme_list/theme_list.html', context={"themes": themes})
