115c77b55193bdf96404c7caf877b1e140e9512e
Denis Kabanin <kabanindenis9@gmail.com>
2023-05-07 13:21:50 +0300

    bugs fix

--- portal/static/js/theme_list/theme_list.js
+++ portal/static/js/theme_list/theme_list.js
@@ -1,24 +1,35 @@
 $('.theme-btn').on('click', function() {
     let block = this.parentNode.parentNode;
-    let descr_length = block.childNodes[5].childNodes.length;
-    let btn;
-
-    if (descr_length === 9) {
-        btn = block.childNodes[5].childNodes[7];
-    }
+    let btn = block.childNodes[3].childNodes[9];
+    let theme_name = block.childNodes[3].childNodes[1];
+    let theme_descr = block.childNodes[3].childNodes[3];
 
     if ($(block).hasClass('theme-block-open')) {
 
+        $(theme_name).addClass('theme-name-close');
+        $(theme_name).removeClass('theme-name-open');
+
+        $(theme_descr).addClass('theme-descr-close');
+        $(theme_descr).removeClass('theme-descr-open');
+
         $(block).addClass('theme-block-close');
         $(block).removeClass('theme-block-open');
+
         $(this).html('Подробнее');
-        $(btn).css({'display': 'none'});
+        // $(btn).css({'display': 'none'});
 
     } else if ($(block).hasClass('theme-block-close')) {
 
+        $(theme_name).addClass('theme-name-open');
+        $(theme_name).removeClass('theme-name-close');
+
+        $(theme_descr).addClass('theme-descr-open');
+        $(theme_descr).removeClass('theme-descr-close');
+
         $(block).addClass('theme-block-open');
         $(block).removeClass('theme-block-close');
+
         $(this).html('Скрыть');
-        $(btn).css({'display': 'block'});
+        // $(btn).css({'display': 'block'});
     }
 });
