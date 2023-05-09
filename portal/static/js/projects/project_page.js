c058a2a8a21430fb73272a86ba8e5258ac8cd59b
XameleonchikGreen <budnikov_nikita@list.ru>
2023-05-09 12:09:08 +0300

    Old projects window

---
+++ portal/static/js/projects/project_page.js
@@ -0,0 +1,9 @@
+function old_files() {
+    $('.old-files').css({ 'display': 'block' });
+    $('.back-form').css({ 'display': 'block' });
+}
+
+function exit() {
+    $('.old-files').css({ 'display': 'none' });
+    $('.back-form').css({ 'display': 'none' });
+}
