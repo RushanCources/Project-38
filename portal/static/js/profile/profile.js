f464b60a3ff6b1d12f57314a65f2ef403b06f411
AdLeGeR <dmitr1effal@yandex.ru>
2023-05-20 15:07:41 +0300

    добавил мигающую цифру

--- portal/static/js/profile/profile.js
+++ portal/static/js/profile/profile.js
@@ -70,17 +70,6 @@ $('.now-project-btn').on('click', function(){
     $('.back-form').css({'display':'block'});
 });
 
-// set open and close to future btn
-$('.future-projects-close').on('click', function() {
-    $('.now-projects-form').css({'display':'none'});
-    $('.back-form').css({'display':'none'});
-});
-
-$('.future-project-btn').on('click', function(){
-    $('.now-projects-form').css({'display':'flex'});
-    $('.back-form').css({'display':'block'});
-});
-
 // set open and close to new btn
 $('.new-projects-close').on('click', function() {
     $('.new-projects-form').css({'display':'none'});
