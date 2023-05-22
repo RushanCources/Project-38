aa011671562e7116a8534a39d57efb3f41ff6f5a
AdLeGeR <dmitr1effal@yandex.ru>
2023-05-22 18:14:05 +0300

    добавил баг(вернул announcements в исходное состояние)

--- portal/announcements/forms.py
+++ portal/announcements/forms.py
@@ -9,7 +9,7 @@ class AnnouncementForm(forms.Form):
     title = forms.CharField(label='', required=True, widget=forms.TextInput(attrs={'class': 'title', 'placeholder' : 'Заголовок объявления'}))
     body = forms.CharField(label='', widget=forms.Textarea(attrs={'class': 'body'}))
     is_pinned = forms.BooleanField(label='Закрепить', required=False)
-    # image_url = forms.FilePathField(label='Выбрать обложку', path='static/img/announcements/covers', required=False, widget=forms.TextInput(attrs={'class': 'imurl'}))
+    image_url = forms.FilePathField(label='Выбрать обложку', path='static/img/announcements/covers', required=False, widget=forms.TextInput(attrs={'class': 'imurl'}))
     files = forms.FileField(label='Прикрепить файлы', widget=forms.FileInput(attrs={'class':"files"}), required=False)
     file_id_to_delete = forms.IntegerField(widget=forms.HiddenInput(attrs={'class': 'fitd'}), required=False)
 
