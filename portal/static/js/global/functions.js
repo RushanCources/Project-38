e1a7790903fe15b830cf4b82ae579c4333e3794c
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2023-08-10 15:09:44 +0300

    added some images to the buttons

--- portal/static/js/global/functions.js
+++ portal/static/js/global/functions.js
@@ -1,19 +1,37 @@
-function genDiv(num, parent, parentName, child, childName, cname) {
+function genDiv(num, parent, cname, parentName=null, child=null, childName=null) {
     // let cont = document.createElement("div");
     // cont.id = cname;
     // document.body.appendChild(cont);
     let bget = document.getElementById(cname);
+    let cget;
     console.log('first let')
     for (let i = 0; i < num; i++) {
         console.log(`${i} let in cicle`)
         let block = document.createElement(parent);
-        block.classList.add(parentName);
-        block.id = `bl${i}`;
-        let bchild = document.createElement(child);
-        bchild.classList.add(childName);
+        if(parentName==null){
+            block.classList.add(`parentName`);
+            block.id = `parentName${i}`;
+        }else{
+            block.classList.add(parentName);
+            block.id = `${parentName}${i}`;
+        }
         bget.appendChild(block);
-        let cget = document.getElementById(`bl${i}`);
-        cget.appendChild(bchild);
+        if (parentName == null) {
+            cget = document.getElementById(`parentName${i}`);
+        } else {
+            cget = document.getElementById(`${parentName}${i}`);
+        }
+        if(child!=null){
+            let bchild = document.createElement(child);
+            if(childName==null){
+                bchild.id = `childName${i}`;
+                bchild.classList.add(`childName`);
+            }else{
+                bchild.id = `${childName}${i}`;
+                bchild.classList.add(childName);
+            }
+            cget.appendChild(bchild);
+        }
     }
 }
 
