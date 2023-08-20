1772804784d6b5463f911c03329c881c3c4b6502
Quenty-Du <132583715+Quenty-Du@users.noreply.github.com>
2023-08-20 19:44:03 +0300

    Commit V.2

--- portal/subjects/models.py
+++ portal/subjects/models.py
@@ -4,7 +4,7 @@ class Theme(models.Model):
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=30)
     type = models.CharField(max_length=10)
-    subject = models.CharField(max_length=20)
+    subjects = models.CharField(max_length=100)
     status = models.CharField(max_length=20)
     descript = models.CharField(max_length=2000)
     class_of_subject = models.CharField(max_length=100)
--- portal/subjects/views.py
+++ portal/subjects/views.py
@@ -10,3 +10,13 @@ def without_filter(request):
 def create_auto(request):
     theme = Theme.objects.create(Name = "123", Author = "123", Subject = "123", Status = "Свободно", Descript = "Тут что-то", Class_of_subject = "theme-block theme-block-close sub2", Class_of_tag = "teg teg-open")
     return redirect('theme_list')
+
+def new_theme_create(request):
+    if request.methood == 'POST':
+        theme = Theme()
+        theme.name = request.POST.get('name_input')
+        theme.descript = request.POST.get('descript_input')
+        theme.subjects = request.POST.get('subjects_input')
+        theme.author = request.POST.get('author_input')
+        theme.save()
+    return redirect('theme_list')
