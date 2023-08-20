821d37a249ae218a194fe5fcde3863fdd77623c3
Quenty-Du <132583715+Quenty-Du@users.noreply.github.com>
2023-08-20 18:55:10 +0300

    Commit V.1
    - migrations have done

--- portal/subjects/models.py
+++ portal/subjects/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Theme(models.Model):
-    Name = models.CharField(max_length=100)
-    Author = models.CharField(max_length=30)
-    Type = models.CharField(max_length=10)
-    Subject = models.CharField(max_length=20)
-    Status = models.CharField(max_length=20)
-    Descript = models.CharField(max_length=2000)
-    Class_of_subject = models.CharField(max_length=100)
-    Class_of_tag = models.CharField(max_length=100, null=True)
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=30)
+    type = models.CharField(max_length=10)
+    subject = models.CharField(max_length=20)
+    status = models.CharField(max_length=20)
+    descript = models.CharField(max_length=2000)
+    class_of_subject = models.CharField(max_length=100)
+    class_of_tag = models.CharField(max_length=100, null=True)
