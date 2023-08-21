57d05c0e1f1b9f24acd3584e1c9e02060e1e3493
Quenty-Du <132583715+Quenty-Du@users.noreply.github.com>
2023-08-21 23:39:35 +0300

    На странице с темами сделан поиск. Работает

--- portal/subjects/urls.py
+++ portal/subjects/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('theme_list/', views.without_filter, name='theme_list'),
     path('theme_create_test/' , views.create_test, name='create_auto'),
     path('new_theme_create/' , views.new_theme_create, name='new_theme_create'),
+    path('search_results_view', views.search, name='search_results_view'),
 ]
--- portal/subjects/views.py
+++ portal/subjects/views.py
@@ -37,3 +37,9 @@ def new_theme_create(request):
         
         theme.save()
     return redirect('theme_list')
+
+def search(request):
+    if request.method == 'POST':
+        search_object = request.POST.get('search_input')
+        search_result = Theme.objects.filter(name__icontains=search_object)
+    return render(request, 'theme_list/theme_list.html' , {'themes': search_result})
