27595e55b4b08b74a89b34c50e8ac61b15532786
Quenty-Du <132583715+Quenty-Du@users.noreply.github.com>
2023-08-23 19:57:46 +0300

    Исправил косяк с переадресацией при регистрации

--- portal/subjects/urls.py
+++ portal/subjects/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from subjects import views
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'), name='subject'),
     path('theme_list/', views.without_filter, name='theme_list'),
     path('theme_create_test/' , views.create_test, name='create_auto'),
     path('new_theme_create/' , views.new_theme_create, name='new_theme_create'),
