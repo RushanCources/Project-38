dfc369e6ad40e2f1e94c9d6792dbdd861b749588
Denis laptop <kabanindenis9@gmail.com>
2023-08-26 15:46:55 +0300

    Продолжил работу над формой

--- portal/announcements/forms.py
+++ portal/announcements/forms.py
@@ -6,9 +6,9 @@ from .models import Announcement
 
 
 class AnnouncementForm(forms.Form):
-    date_of_expiring = forms.DateField(label='', widget=forms.DateInput(attrs={'class': 'date', 'type': 'date'}), required=False)
-    title = forms.CharField(label='', required=True, widget=forms.Textarea(attrs={'class': 'label-textarea', 'placeholder' : 'Заголовок объявления', 'onkeyup' : 'textarea_size(this)', 'cols' : 'none', 'rows' : 'none'}))
-    body = forms.CharField(label='', widget=forms.Textarea(attrs={'class': 'label-textarea', 'onkeyup' : 'textarea_size(this)', 'cols' : 'none', 'rows' : 'none'}))
+    date_of_expiring = forms.DateField(label='', widget=forms.DateInput(attrs={'class': 'date', 'type': 'date', 'onchange' : 'preview_update(this, \'date\')'}), required=False)
+    title = forms.CharField(label='', required=True, widget=forms.Textarea(attrs={'class': 'label-textarea', 'placeholder' : 'Заголовок объявления', 'onkeyup' : 'textarea_size(this); preview_update(this, \'title\')', 'cols' : 'none', 'rows' : 'none'}))
+    body = forms.CharField(label='', widget=forms.Textarea(attrs={'class': 'label-textarea', 'onkeyup' : 'textarea_size(this); preview_update(this, \'descr\')', 'cols' : 'none', 'rows' : 'none'}))
     is_pinned = forms.BooleanField(label='Закрепить', required=False)
     image_url = forms.FilePathField(label='Выбрать обложку', path=finders.find("img/announcements/covers"), required=False, widget=forms.TextInput(attrs={'class': 'imurl'}))
     files = forms.FileField(label='Прикрепить файлы', widget=forms.FileInput(attrs={'class':"file-input", "id" : 'file', "multiple" : ''}), required=False)
--- portal/static/js/announcement/red.js
+++ portal/static/js/announcement/red.js
@@ -7,9 +7,11 @@ function pin_active() {
     if ($('.div-pin').hasClass('div-pin-active')) {
         $('.div-pin').removeClass("div-pin-active");
         $('.ball').css({ 'transform': 'translateX(0px)' });
+        preview_update(false, 'pin');
     } else {
         $('.div-pin').addClass("div-pin-active");
         $('.ball').css({ 'transform': 'translateX(24px)' });
+        preview_update(true, 'pin');
     }
 }
 
@@ -18,17 +20,23 @@ let data = new DataTransfer();
 function new_file() {
     let new_file = $('#add_file')[0].files[0];
     let ul = $('.file-list')[0];
+    let pre_ul = $('.announ-files-list')[0];
     let li = document.createElement('li');
+    let pre_li = document.createElement('li');
     let div = document.createElement('div');
     let text = new_file.name;
 
     $(li).attr('title', text);
-
+    $(pre_li).attr('title', text);
     text = file_name(text);
+    $(pre_li).attr('class' , 'announ-files-item');
 
     $(li).attr('class', 'file-item');
     $(div).attr('class', 'file-item-div')
     li.innerText = text;
+    pre_li.innerText = text;
+
+    pre_ul.append(pre_li);
 
     ul.append(li);
     li.append(div);
@@ -75,4 +83,48 @@ function file_remove() {
             }
         }
     });
+}
+
+$('.covers-item').on('click', function() {
+    let cls = $(this).attr('class');
+    cls = cls.replace('covers-item ', '.');
+
+    let url = $(cls).css('background-image');
+
+    $('.covers-selected').css({'display' : 'none'});
+    $(this.childNodes[1]).css({'display' : 'flex'});
+
+    preview_update(url, 'cover');
+});
+
+let date = new Date();
+
+let today = date.getDate();
+let months = [' января ', ' февраля ', ' марта ', ' апреля ', ' мая ', ' июня ', ' июля ', ' августа ', ' сентября ', ' октября ', ' ноября ', ' декабря '];
+let month = months[date.getMonth()];
+let year = date.getFullYear();
+$('.date-start').html(today + month + year);
+
+function preview_update(el, block) {
+    if (block == 'title') {
+        let text = $(el).val();
+        $('.announ-title').html(text);
+    } else if (block == 'descr') {
+        let text = $(el).val();
+        $('.announ-descr').html(text);
+    } else if (block == 'date') {
+        let date = $('.date').val();
+        let date_arr = date.split('-');
+        let month = months[Number(date_arr[1]) - 1];
+        $('.date-end').html(date_arr[2] + month + date_arr[0]);
+    } else if (block == 'pin') {
+        if (el) {
+            $('.favourite-btn').css({'fill' : '#E4CE02'});
+        } else {
+            $('.favourite-btn').css({'fill' : '#DBDBDB'});
+        }
+    } else if (block == 'cover') {
+        console.log(el);
+        $('.announ-img').css({'background-image' : el});
+    }
 }
