3a9a45a9b7f9a8dde07f45f2e1b64e26a830daf1
Denis Kabanin <kabanindenis9@gmail.com>
2023-08-28 19:24:26 +0300

    обновил ui редактора объявлений

---
+++ portal/static/js/announcement/ed.js
@@ -0,0 +1,15 @@
+let pin = $('#id_is_pinned')[0];
+
+if (pin.checked) {
+    $('.div-pin').addClass("div-pin-active");
+    $('.ball').css({ 'transform': 'translateX(24px)' });
+    preview_update(true, 'pin');
+}
+
+let title = $('.label-textarea')[0];
+let descr = $('.label-textarea')[1];
+let dte = $('.date')[0];
+
+preview_update(title, 'title');
+preview_update(descr, 'descr');
+preview_update(dte, 'date');
