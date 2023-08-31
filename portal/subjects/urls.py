e66ccb3ed564613802a44d68ea000c12aaab4cec
Quenty-Du <132583715+Quenty-Du@users.noreply.github.com>
2023-08-31 11:28:53 +0300

    Студенты хогвартса прилетели на метлах и сделали фильтры с передачей данных из subject-filter

--- portal/subjects/urls.py
+++ portal/subjects/urls.py
@@ -3,8 +3,7 @@ from subjects import views
 
 urlpatterns = [
     path('theme_list/', views.without_filter, name='theme_list'),
-    path('theme_create_test/' , views.create_test, name='create_auto'),
-    path('new_theme_create/' , views.new_theme_create, name='new_theme_create'),
+    path('new_theme_create/', views.new_theme_create, name='new_theme_create'),
     path('search_results_view', views.search, name='search_results_view'),
     path(route='use_theme', view=views.use_theme, name='use_theme'),
 ]
